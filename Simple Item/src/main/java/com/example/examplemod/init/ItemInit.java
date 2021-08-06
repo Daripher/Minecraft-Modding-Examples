bf2de7ec935016ca089316eca6a14c48b3aad429
Daripher <saelver.serbek@gmail.com>
2021-08-06 16:48:01 +0500

    Added simple item example

---
+++ Simple Item/src/main/java/com/example/examplemod/init/ItemInit.java
@@ -0,0 +1,17 @@
+package com.example.examplemod.init;
+
+import com.example.examplemod.ExampleMod;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
+import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemInit
+{
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MOD_ID);
+	
+	public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Properties().tab(CreativeModeTab.TAB_MISC)));
+}
